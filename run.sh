3f310b002a04ce74cb238dec5dc52568258ba816
luckylinux <luckylinux>
2024-06-12 22:15:51 +0200

    Add custom Options and Capabilities.

--- run.sh
+++ run.sh
@@ -21,6 +21,17 @@ containerimage="docker-network-troubleshooting:debian-latest"
 # Containers Configuration Folder to Map
 containersconfigfolder="./containers"
 
+# Options
+opts=()
+
+# Podman 5.x with Pasta doesn't handle Networking Correctly
+# Force to use slirp4netns
+opts+=("--network=slirp4netns")
+
+# Add Capacilities
+opts+=("--cap-add")
+opts+=("CAP_NET_RAW")
+
 # Load the Environment Variables into THIS Script
 if [[ -f "./.env" ]]
 then
@@ -33,7 +44,7 @@ ${engine} rm --ignore ${containername}
 
 # Run Image with Infinite Loop to prevent it from automatically terminating
 #${engine} run --name=${containername} --env-file "./.env" localhost:5000/local/"${containerimage}"
-${engine} run --name=${containername} localhost:5000/local/"${containerimage}"
+${engine} run --name=${containername} ${opts[*]} localhost:5000/local/"${containerimage}"
 
 # Open Interactive Shell with Container
 ${engine} exec -it ${containername} /bin/bash
