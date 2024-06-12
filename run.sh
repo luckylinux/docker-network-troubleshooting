60dba0d2cbfa13feac5aaa839cbeb9aef603322a
luckylinux <luckylinux>
2024-06-12 22:09:55 +0200

    Adapted run.sh for the new Image.

--- run.sh
+++ run.sh
@@ -13,27 +13,27 @@ source ${libpath}/functions.sh
 engine=${1-"podman"}
 
 # Container Name
-containername="container-registry-tools"
+containername="docker-network-troubleshooting"
 
 # Container Image
-containerimage="container-registry-tools:debian-latest"
+containerimage="docker-network-troubleshooting:debian-latest"
 
 # Containers Configuration Folder to Map
 containersconfigfolder="./containers"
 
 # Load the Environment Variables into THIS Script
-eval "$(shdotenv --env .env || echo \"exit $?\")"
+if [[ -f "./.env" ]]
+then
+   eval "$(shdotenv --env .env || echo \"exit $?\")"
+fi
 
 # Terminate and Remove Existing Containers if Any
 ${engine} stop --ignore ${containername}
 ${engine} rm --ignore ${containername}
 
 # Run Image with Infinite Loop to prevent it from automatically terminating
-${engine} run --privileged -d --name=${containername} --env-file "./.env" -v "./regctl:/etc/regctl" -v "${containersconfigfolder}:/etc/containers" -v "./supercronic:/etc/supercronic" localhost:5000/local/"${containerimage}"
+#${engine} run --name=${containername} --env-file "./.env" localhost:5000/local/"${containerimage}"
+${engine} run --name=${containername} localhost:5000/local/"${containerimage}"
 
 # Open Interactive Shell with Container
 ${engine} exec -it ${containername} /bin/bash
-
-# Sync One Image
-#${engine} exec "${containername}" skopeo sync --scoped --src docker --dest docker --all ghcr.io/home-assistant/home-assistant:stable "${LOCAL_MIRROR}" # Double Quotes means that the value from the HOST Shell will be used
-#${engine} exec "${containername}" skopeo sync --scoped --src "docker" --dest "docker" --all "ghcr.io/home-assistant/home-assistant:stable" '${LOCAL_MIRROR}'    # Single Quotes means that the value from the CONTAINER Shell will be used
